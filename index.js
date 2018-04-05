ce01b36a175bbc6c556db2da2ca02ae30e3463ff
WeerakoonOS <osw0294@gmail.com>
2018-04-05 18:30:30 +0530

    JSON object with unit=metric

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ let request = require('request');
 
 let apiKey = 'eb59071d486d6c9ef2159f4c067b5fee';
 let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+    //console.log('body:', body);
   }
 });
