8fbf0096a40e98c0bcae051a540d09414c4fd956
WeerakoonOS <osw0294@gmail.com>
2018-04-05 18:50:03 +0530

    simple command line app

---
+++ simple_app.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = 'eb59071d486d6c9ef2159f4c067b5fee';
+let city = argv.c || 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} Celcius in ${weather.name}!`;
+    console.log(message);
+    //console.log('body:', body);
+  }
+});
