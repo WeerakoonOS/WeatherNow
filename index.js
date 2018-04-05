fbc88d07a4af4b9ac399cd1790be11f5c945bfc6
WeerakoonOS <osw0294@gmail.com>
2018-04-05 18:42:48 +0530

    Adding in interactivity

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-let request = require('request');
+const request = require('request');
+const argv = require('yargs').argv;
 
 let apiKey = 'eb59071d486d6c9ef2159f4c067b5fee';
-let city = 'portland';
+let city = argv.c || 'portland';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
